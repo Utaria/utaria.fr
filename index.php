b5326723f335f13336341065f7b592f6d9d1db40
utarwyn <maxime.malgorn@laposte.net>
2018-05-05 10:47:01 +0200

    Nouvelle page accueil (NextGen) + Règlementation CNIL

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 define("BASE", rtrim(dirname($_SERVER["SCRIPT_NAME"]), "/"));
 
-$title = "Ça y est, on est là !";
+$title = "Communauté Minecraft.";
 ?>
 <!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
 <!-- TU FAIS QUOI LÀ JEUNE PADAWAN ???                             -->
@@ -39,48 +39,70 @@ $title = "Ça y est, on est là !";
 	    <meta property="og:url" content="https://utaria.fr/">
 
 	    <link rel="icon" type="image/png" href="<?= BASE ?>/img/favicon.png?v=2" />
-	
+
 		<meta name="viewport" content="width=device-width, initial-scale = 1, user-scalable = no">
 
 		<link href="//fonts.googleapis.com/css?family=Lato:400,600" rel="stylesheet">
         <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
-		<link href="<?= BASE ?>/css/timecircles.css?v=2.2" rel="stylesheet">
-		<link href="<?= BASE ?>/css/app.css?v=2.2" rel="stylesheet">
+		<link href="<?= BASE ?>/css/timecircles.css?v=05052018" rel="stylesheet">
+		<link href="<?= BASE ?>/css/app.css?v=05052018" rel="stylesheet">
 	</head>
 	<body>
+        <!-- Bannière d'acceptation/rejet des cookies (miam) (Règlementation CNIL) -->
+        <div id="cookies-eu-banner" style="display: none;">
+            En poursuivant votre navigation sur ce site, vous acceptez l'utilisation de cookies par Google Analytics pour réaliser des statistiques de visites.
+            <a href="./en-savoir-plus.html" id="cookies-eu-more" style="display:none">En savoir plus</a>
+            <button id="cookies-eu-reject">Je refuse</button>
+            <button id="cookies-eu-accept">J'accepte</button>
+        </div>
+
 		<section class="content wrapper">
-			<img class="logo" src="<?= BASE ?>/img/logo.png?v=2.2" alt="Logo d'Utaria">
-			<span class="badge">V2</span>
+			<img class="logo" src="<?= BASE ?>/img/logo.png?v=05052018" alt="Logo d'Utaria">
+			<!-- <span class="badge">V3</span> -->
 
-            <h1><?= $title ?> <i class="em em---1"></i> <i class="em em-fire"></i></h1>
+            <h1><?= $title ?></h1>
+            <h2>On cherche à faire du nouveau !</h2>
 
-            <button data-clipboard-text="mc.utaria.fr" onclick="alert('IP copiée !');" class="button-ip">
-                mc.utaria.fr
-            </button>
+            <div class="button-ip-container">
+                <div class="button-ip-top">
+                    Le faction du futur !
+                </div>
+                <button data-clipboard-text="nextgen.utaria.fr" onclick="animationBoutonIp(event)" class="button-ip">
+                    <span class="ip">nextgen.utaria.fr</span>
+                    <span class="copied">Ip copiée !</span>
+                </button>
+            </div>
 
-            <!-- <div class="v2cooldown" data-date="2018-03-03 14:00:00"></div> -->
-            <!-- Oui, ce coup-ci c'est bien la bonne date ! ;-) -->
+            <!-- <div class="v2cooldown" data-date="2018-03-03 17:00:00"></div> -->
+            <!-- C'était l'heure d'ouverture de notre Survie ! :'( RIP -->
 
             <div class="socials">
                 <a class="discord" href="https://discord.gg/UNgPrPk" target="_blank"></a>
                 <a class="twitter" href="https://twitter.com/Utaria_FR" target="_blank"></a>
+                <a class="github"  href="https://github.com/Utaria" target="_blank"></a>
                 <a class="fb"      href="https://facebook.com/utaria.fr/" target="_blank"></a>
             </div>
 
 		</section>
 
-		<script type="text/javascript" src="<?= BASE ?>/js/jquery.min.js?v=2.1"></script>
-		<script type="text/javascript" src="<?= BASE ?>/js/backstretch.js?v=2.1"></script>
-		<script type="text/javascript" src="<?= BASE ?>/js/timecircles.js?v=2.1"></script>
+		<script type="text/javascript" src="<?= BASE ?>/js/jquery.min.js?v=05052018"></script>
+        <script type="text/javascript" src="<?= BASE ?>/js/backstretch.js?v=05052018"></script>
+        <script type="text/javascript" src="<?= BASE ?>/js/cookies-cnil.js?v=05052018"></script>
+		<script type="text/javascript" src="<?= BASE ?>/js/timecircles.js?v=05052018"></script>
 
+        <!-- Grâce à ce petit code, on peut savoir qui vient sur notre site :) -->
 		<!-- Global site tag (gtag.js) - Google Analytics -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=UA-87706617-1"></script>
         <script type="text/javascript">
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+            new CookiesEuBanner(function() {
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                    dataLayer.push(arguments);
+                }
 
-          gtag('config', 'UA-87706617-1');
+                gtag('js', new Date());
+                gtag('config', 'UA-87706617-1');
+            }, true);
         </script>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js"></script>
@@ -89,12 +111,13 @@ $title = "Ça y est, on est là !";
             $(document).ready(function() {
                 // Image de fond dynamique
                 $.backstretch(
-                    ["<?= BASE ?>/img/001.jpg","<?= BASE ?>/img/002.jpg","<?= BASE ?>/img/003.jpg"],
+                    ["<?= BASE ?>/img/004.jpg","<?= BASE ?>/img/001.jpg","<?= BASE ?>/img/005.jpg","<?= BASE ?>/img/002.jpg","<?= BASE ?>/img/006.jpg","<?= BASE ?>/img/003.jpg","<?= BASE ?>/img/007.jpg"],
                     //["<?= BASE ?>/img/d1.jpg","<?= BASE ?>/img/d2.jpg","<?= BASE ?>/img/d3.jpg","<?= BASE ?>/img/d4.jpg","<?= BASE ?>/img/d5.jpg","<?= BASE ?>/img/d6.jpg"],
                     { fade: 750, duration: 2500 }
                 );
 
                 // Démarrage du timer (cooldown)
+                /*
                 $(".v2cooldown").TimeCircles({
                     animation: "tricks",
                     bg_width: 0,
@@ -129,10 +152,29 @@ $title = "Ça y est, on est là !";
                 $(window).resize(function() {
                     $(".v2cooldown").TimeCircles().rebuild();
                 });
+                */
 
+                // Bouton de copie de l'adresse IP !
                 new ClipboardJS('.button-ip');
-
             });
+
+            // Animation de copie de l'IP
+            var canClick = true;
+            function animationBoutonIp(event) {
+                var target = event.target || event.srcElement || event.delegateTarget;
+                if (target == null || !canClick) return;
+
+                if (target.tagName === "SPAN")
+                    target = target.parentNode;
+
+                target.classList.add("clicked");
+                canClick = false;
+
+                setTimeout(function() {
+                    target.classList.remove("clicked");
+                    canClick = true;
+                }, 3000);
+            }
         </script>
 
 	</body>
